36ff2caa3d09e16411ab05e93465a184440c47ef
Chris <ChristoWindow@gmail.com>
2024-10-01 12:44:24 -0400

    AddedSpringBoot

---
+++ JavaCrud/src/test/java/org/example/javacrud/JavaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javacrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
