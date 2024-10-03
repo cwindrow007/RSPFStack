d11399f5525cbacb59652f49be619699f834aaae
Chris <ChristoWindow@gmail.com>
2024-10-03 19:05:38 -0400

    Created MainPage

---
+++ frontend/src/Pages/MainPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainPage: React.FC = () => {
+    return(
+        <div>
+            <h1>Welcome to the RSPF Inventory Management System</h1>
+            <p>This page is yet to be filled but for now it is in testing before design and layout</p>
+        </div>
+    );
+};
+
+export default MainPage;
