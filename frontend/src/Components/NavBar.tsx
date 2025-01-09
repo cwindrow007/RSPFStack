87c074a1874116d37cef612334480473e98d198d
Chris <ChristoWindow@gmail.com>
2025-01-09 18:53:40 -0500

    Icons Work

--- frontend/src/Components/NavBar.tsx
+++ frontend/src/Components/NavBar.tsx
@@ -1,40 +1,66 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+
+//Import Icons
+
+import HomeIcon from "../Icons/Home.png";
+import ProductsIcon from "../Icons/Products.png";
+import OrdersIcon from "../Icons/Orders.png";
+import InventoryIcon from "../Icons/Inventory.png";
+import InvoicesIcon from "../Icons/Invoices.png";
+import DataIcon from "../Icons/Data.png";
+
+
 /** Nav Bar, holds data to links on the left side and program logo*/
 const NavBar = () =>{
     const linker = [
         {
             to: '/',
-            name: 'Main'
+            name: 'Main',
+            icon: HomeIcon
         },
         {
             to: '/Products',
-            name: "Products"
+            name: "Products",
+            icon: ProductsIcon
         },
         {
             to: '/Orders',
-            name: "Orders"
+            name: "Orders",
+            icon: OrdersIcon
         },
         {
             to: '/Inventory',
-            name: "Inventory"
+            name: "Inventory",
+            icon: InventoryIcon
         },
         {
             to: '/Invoices',
-            name: "Invoices"
+            name: "Invoices",
+            icon: InvoicesIcon
         },
         {
             to: '/Data',
-            name: "Data"
+            name: "Data",
+            icon: DataIcon
         },
 
 
     ]
     return(
         <div className="NavBar">
-            <h1>RSPF Inventory System</h1>
+            {/*Program Logo */}
+            <h1 className = "Placeholder">
+                RSPF Inventory System
+            </h1>
             <div>
-                {linker.map(link=>(<Link to={link.to}><li>{link.name}</li></Link>))}
+                {linker.map(link=>(
+                    <Link to={link.to}>
+                    <ul>
+                        <img src = {link.icon} alt = {link.name}/>
+                        {link.name}
+                    </ul>
+                </Link>))}
             </div>
         </div>
     );
