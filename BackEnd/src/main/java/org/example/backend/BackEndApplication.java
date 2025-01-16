c38f6586277cb4ea8fc90d3204488c0740e5a276
Chris <ChristoWindow@gmail.com>
2025-01-16 10:14:22 -0500

    Updates to BackEnd

--- BackEnd/src/main/java/org/example/backend/BackEndApplication.java
+++ BackEnd/src/main/java/org/example/backend/BackEndApplication.java
@@ -1,5 +1,6 @@
 package org.example.backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackEndApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+
+        //System Properties for Springboot to connect to DataBase
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
         SpringApplication.run(BackEndApplication.class, args);
     }
 
