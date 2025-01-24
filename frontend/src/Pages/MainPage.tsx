98cc112f35fe7124f8729956f9f88c8312c54b85
Chris <ChristoWindow@gmail.com>
2025-01-24 10:36:02 -0500

    Proxy bug fix. Tailwind Styling
    Fixed the proxy bug that had been occurring that prevented the react app from starting. Styling done to main page to "center" main contents and keep other components from being affected.

--- frontend/src/Pages/MainPage.tsx
+++ frontend/src/Pages/MainPage.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
-import {Link} from "react-router-dom";
 import LeftNav from "../Components/NavBar";
 import TopBar from "../Components/TopBar"
 
 const MainPage: React.FC = () => {
     return(
-        <div>
-            <LeftNav/>
-            <h1 className="text-3xl text-blue-500 font-bold">Welcome to the RSPF Inventory Management System</h1>
-            <p>This page is yet to be filled but for now it is in testing before design and layout</p>
+        <div className="flex h-screen">
+            <aside className="flex-shrink-0">
+                <TopBar/>
+                <LeftNav/>
+            </aside>
+            <main className = "flex-1 flex items-center justify-center">
+                <div className="text-center">
+                    <h1 className="text-3xl text-blue-500  underline font-bold">
+                        Welcome to the RSPF Inventory Management System
+                    </h1>
+                    <p>
+                        This page is yet to be filled but for now it is in testing before design and layout
+                    </p>
+                </div>
+            </main>
         </div>
     );
 };
