13c43171ee8e2bad49977f7e7ce4d1e57f54ee3f
Chris <ChristoWindow@gmail.com>
2025-01-24 10:48:03 -0500

    Products Page Styling
    Products page got a small amount of styling.  Button is centered and needs to be top right. Below Products info needs to be a table. Reference: Layout ex in Obsidian on Desktop.

--- frontend/src/Pages/Products.tsx
+++ frontend/src/Pages/Products.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import TopNav from "../Components/TopBar";
 import LeftNav from "../Components/NavBar";
-import TableCreation from "../Components/Modals/CreateTableModal";
+import TableCreationButton from "../Components/Modals/CreateTableModal";
 
 const Products: React.FC = () => {
     return(
-        <div>
-            <TopNav/>
-            <LeftNav/>
-            <h1 className = "text-2xl text-black font-bold">Products</h1>
-            <p>This is the Products Page</p>
-            <TableCreation/>
+        <div className="flex h-screen">
+            <aside className = "flex-shrink-0">
+                <TopNav/>
+                <LeftNav/>
+            </aside>
+            <main className = "flex-1 flex items-center justify-center" >
+                <div className = "text-center" >
+                    <h1 className="text-2xl font-bold underline">
+                        Products
+                    </h1>
+                    <p>Products Info</p>
+                    <TableCreationButton/>
+                </div>
+            </main>
         </div>
 
     );
