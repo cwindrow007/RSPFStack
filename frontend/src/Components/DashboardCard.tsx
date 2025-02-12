60429cdf0432a854d9d786924d2ae97ed652a5f6
Chris <100327446+cwindrow007@users.noreply.github.com>
2025-02-12 10:24:13 -0500

    Update DashboardCard.tsx
    Text Styling more, Needs work per usual. Work be done once home

--- frontend/src/Components/DashboardCard.tsx
+++ frontend/src/Components/DashboardCard.tsx
@@ -17,10 +17,10 @@ export function DashboardCard({title, Description, buttonText, icon, route}: bas
         <div className = "relative border rouded-lg shadow-md p-6 bg-white hover:shadow-lg cursor-pointer flex items-center">
             <div>
                 <h3 className = "text-xl font-semibold">{title}</h3>
-                <p className = "text-gray-600 my-2">{Description}</p>
+                <p className = "text-gray-600 mt-2">{Description}</p>
                 <div className = "absolute top-3 right-3 text-2xl text-gray-500 ">{icon}</div>
 
-                <div>
+                <div className="mt-10 flex justify-center">
                     <button
                     className="bg-blue-500 text-white px-4 py-2 rouded hover:bg-blue-600 transition"
                         onClick={() => navigate(route)}>
